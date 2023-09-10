f3b31f21a729b372178a0ce3614c64c917fd6d2e
serocan38 <s.o.gs@hotmail.com>
2023-09-10 23:59:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Box } from "@mui/material";
+import MainPage from "./components/mainpage";
 
 function App() {
+  const [isLoading, setIsLoading] = useState();
+  const [hero, setHero] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    if (hero) console.log(hero);
+  }, [hero]);
+
+  const fetchData = async (path) => {
+    const response = await fetch(path);
+    if (response.status === 200) {
+      const data = await response.json();
+      return data;
+    }
+    alert("Api hatasi");
+    return;
+  };
+
+  const getData = async () => {
+    setIsLoading(true);
+    const marvelData = window.localStorage.getItem("marvelData");
+    if (!marvelData) {
+      const data = await fetchData(
+        "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=21c4deff50c8b10883535b83bcb4368e&hash=4a94f81d38fa18041d34358a5424c38b"
+      ).catch((e) => console.error(e));
+      window.localStorage.setItem("marvelData", JSON.stringify(data?.data?.results));
+      setHero(data?.data?.results);
+    } else {
+      setHero(JSON.parse(marvelData));
+    }
+    setIsLoading(false);
+  };
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box height={"100%"}>
+      <MainPage />
+    </Box>
   );
 }
 
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function HomePage() {
+    
+    return(
+        <Box></Box>
+    );
+};
+
+export default HomePage;
